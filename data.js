4c529108b9ce83f29dcd6176ad11efcd861f2d3d
ismnarmin13 <narminvi@code.edu.az>
2024-01-16 01:07:02 +0400

    commit

--- data.js
+++ data.js
@@ -6,9 +6,33 @@ let songs = [
         cover: 'https://img.wapka.org/007p54.jpg'
     },
     {
-        name: 'song2',
+        name: 'Still With You',
+        path: 'music/SWU.mp3',
+        artist: 'JungKook(BTS)',
+        cover: 'https://i.pinimg.com/originals/59/96/eb/5996eb8cf54424fc1df194eb6c3a88c5.png'
+    },
+    {
+        name: 'Antifragile',
         path: 'music/antifragile.mp3',
         artist: 'LE SSERAFIM',
         cover: 'https://www.nme.com/wp-content/uploads/2022/10/le-sserafim-antifragile-171022.jpg'
-    }
+    },
+    {
+        name: 'More',
+        path: 'music/MORE.mp3',
+        artist: 'J-Hope(BTS)',
+        cover: 'https://ibighit.com/bts/images/bts/discography/j-hope/jack-in-the-box/album-cover.png'
+    },
+    {
+        name: 'Memphis Cult – 9MM',
+        path: 'music/off.mp3',
+        artist: 'Groove Dealers',
+        cover: 'https://images.genius.com/ccf1c3825e3fa9d4a1d55d5ca7c9ff94.640x640x1.jpg'
+    },
+    {
+        name: '3D',
+        path: 'music/3D.mp3',
+        artist: 'JungKook(BTS)',
+        cover: 'https://www.allkpop.com/upload/2023/10/content/170652/1697539945-20231017-jungkook.jpg'
+    }  
 ]
